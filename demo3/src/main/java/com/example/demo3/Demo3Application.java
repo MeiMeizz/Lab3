60ccff3623793b528f1424976a5aa2e854797880
Nguyen Thanh Tuan <nguyenthanhtuan300102@gmail.com>
2023-05-24 15:52:31 +0700

    Nop bai tuan 2

--- demo3/src/main/java/com/example/demo3/Demo3Application.java
+++ demo3/src/main/java/com/example/demo3/Demo3Application.java
@@ -2,6 +2,7 @@ package com.example.demo3;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Component;
 
 @SpringBootApplication
 public class Demo3Application {
--- demo3/src/main/java/com/example/demo3/entity/Book.java
+++ demo3/src/main/java/com/example/demo3/entity/Book.java
@@ -1,21 +1,29 @@
 package com.example.demo3.entity;
+import com.example.demo3.Validator.annotation.ValidCategoryId;
 import  jakarta.persistence.*;
-import jdk.jfr.Category;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import  lombok.Data;
+import jakarta.validation.constraints.*;
 @Data
 @Entity
 @Table(name="book")
 public class Book {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private  long id;
+    private  Long id;
     @Column(name = "title")
+    @NotEmpty(message = "Title must not be empty")
+    @Size(max = 50, min = 1,message = "Title must be less than 50 character")
     private String title;
     @Column(name = "author")
     private String author;
     @Column(name = "price")
+    @NotNull(message = "Price is required")
     private Double price;
     @ManyToOne
     @JoinColumn(name = "category_id")
+    @ValidCategoryId
     private Category category;
 }
